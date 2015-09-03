9eb45485e2b38e3d6aa06d9f1d378928200fbb62
vbratsun <vbratsun@gmail.com>
2015-09-04 00:09:39 +0300

    new solution for oop course added

---
+++ OOP/ClassAndObject/ClassAndObject/Batterfly.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObject
+{
+    class Batterfly
+    {
+        //поля(переменная) класса
+        public string batterflyName;
+        public string batterflyColor;
+        public string batterflySize;
+        public int batterflyQuantity;
+
+
+        //методы класса 
+        public void fly()
+        {
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine("Fly"+i);
+        }
+
+        public void eat()
+        {
+            Console.WriteLine("eating now");
+        }
+    }
+}
---
+++ OOP/ClassAndObject/ClassAndObject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObject
+{
+    //объект
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Batterfly Mahaon = new Batterfly();
+            Mahaon.batterflyName = "Mahaon";
+            Mahaon.fly();
+            Mahaon.batterflyColor = "White";
+            Mahaon.batterflySize = "big one";
+            Mahaon.batterflyQuantity = 12;
+            Console.ReadKey();
+        }
+    }
+}
