6e134e46b3816e7277e3d4378512eec8d06895d1
vbratsun <vbratsun@gmail.com>
2015-09-22 23:28:58 +0300

    Lesson2

---
+++ StartUp/Lesson2/Lesson2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 255;
+            short b = 256;
+            double c = 0;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            
+            if (a>0)
+            {
+                c = a + b;
+            }
+            Console.WriteLine(c);
+
+            
+            Console.ReadKey();
+        }
+    }
+}
