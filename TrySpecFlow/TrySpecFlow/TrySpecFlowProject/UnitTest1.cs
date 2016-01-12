d03dd3866f62869450c169e313e0093503558746
vbratsun <vbratsun@gmail.com>
2016-01-12 20:17:45 +0300

    try1

--- TrySpecFlow/TrySpecFlow/TrySpecFlowProject/UnitTest1.cs
+++ TrySpecFlow/TrySpecFlow/TrySpecFlowProject/UnitTest1.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
 
 namespace TrySpecFlowProject
 {
     [TestClass]
     public class UnitTest1
     {
+        
         [TestMethod]
         public void TestMethod1()
         {
+            Driver = new driver();
+            Driver.Navigate().GoToUrl(Wrapper.GetUrl());
+            Assert.IsTrue(Driver.Title == "Яндекс");           
         }
     }
 }
