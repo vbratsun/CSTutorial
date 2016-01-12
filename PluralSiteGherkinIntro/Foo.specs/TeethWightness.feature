c896e08cad296eef5c656ed812b3f22f70668ade
vbratsun <vladimir.bratsun@fourth.com>
2016-01-12 17:04:30 +0300

    Sharing data

---
+++ PluralSiteGherkinIntro/Foo.specs/TeethWightness.feature
@@ -0,0 +1,12 @@
+﻿Feature: TeethWightness
+	
+Scenario Outline: Teeth whiteness
+Given I'm using "<brand>" brand toothpaste
+When i brush  for <mins> minutes
+Then the teeth look <percent> white
+
+Examples: 
+	| brand  | mins | percent |
+	| brand1 | 1    | 80      |
+	| brand2 | 3    | 100     |
+	| brand3 | 10   | 50      |
---
+++ PluralSiteGherkinIntro/Foo.specs/TeethWightnessSteps.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Foo.specs
+{
+    [Binding]
+    public class TeethWightnessSteps
+    {
+        //string _brand;
+
+        [Given(@"I'm using ""(.*)"" brand toothpaste")]
+        public void GivenIMUsingBrandToothpaste(string brand)
+        {
+            ScenarioContext.Current.Add("brand name",brand);
+            //or this can be done this way for private field _brand
+            //_brand = brand;
+        }
+        
+        [When(@"i brush  for (.*) minutes")]
+        public void WhenIBrushForMinutes(int p0)
+        {
+            var brandName = ScenarioContext.Current["brand name"];
+            //var brandName = _brand;
+        }
+        
+        [Then(@"the teeth look (.*) white")]
+        public void ThenTheTeethLookWhite(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
