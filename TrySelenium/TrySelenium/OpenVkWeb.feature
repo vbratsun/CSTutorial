0eb4b0f2db92afee7e27feeb57a87901ba304d1b
vbratsun <vladimir.bratsun@fourth.com>
2016-01-13 17:50:50 +0300

    MessageSending

---
+++ TrySelenium/TrySelenium/OpenVkWeb.feature
@@ -0,0 +1,8 @@
+﻿Feature: OpenVkWeb
+	
+Scenario: Send greeting to friend
+	Given user opens messages section on VK website
+	And user brings up the dialog with friend
+	When some text 'Hello my friend' is typed in  
+	And Send button is clicked
+	Then message is displayed in message section
---
+++ TrySelenium/TrySelenium/OpenVkWebSteps.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using System;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace TrySelenium
+{
+    [Binding]
+    public class OpenVkWebSteps
+    {
+        [Given(@"user opens messages section on VK website")]
+        public void GivenUserOpensMessagesSectionOnVKWebsite()
+        {
+            IWebDriver driver = new InternetExplorerDriver();
+
+            ScenarioContext.Current.Add("driver", driver);
+
+            driver.Navigate().GoToUrl("http://vk.com/im");
+                        
+        }
+        
+        [Given(@"user brings up the dialog with friend")]
+        public void GivenUserBringsUpTheDialogWithFriend()
+        {
+            //var driver = ScenarioContext.Current.Get<IWebDriver>(driver); //??????????????????
+            var driver = ScenarioContext.Current["driver"] as IWebDriver;
+
+            Thread.Sleep(1000);
+
+            driver.FindElement(By.Id("im_dialog18744588")).Click(); 
+        }
+        
+        [When(@"some text '(.*)' is typed in")]
+        public void WhenSomeTextIsTypedIn(string p0)
+        {
+            var driver = ScenarioContext.Current["driver"] as IWebDriver;
+
+            Thread.Sleep(1000);
+
+            var messageEditBox = driver.FindElement(By.Id("im_editable18744588"));
+
+            messageEditBox.SendKeys(p0);
+
+            ScenarioContext.Current.Add("sentMessage", p0);
+        }
+        
+        [When(@"Send button is clicked")]
+        public void WhenSendButtonIsClicked()
+        {
+            var driver = ScenarioContext.Current["driver"] as IWebDriver;
+            
+            Thread.Sleep(1000);
+
+            driver.FindElement(By.Id("im_send")).Click();
+        }
+        
+        [Then(@"message is displayed in message section")]
+        public void ThenMessageIsDisplayedInMessageSection()
+        {
+            var driver = ScenarioContext.Current["driver"] as IWebDriver;
+
+            Thread.Sleep(1000);
+
+            string sentText = driver.FindElement(By.ClassName("im_msg_text")).Text;
+
+            string expectedText = ScenarioContext.Current["sentMessage"] as string;
+
+            Assert.AreEqual(sentText, expectedText);
+        }
+    }
+}
