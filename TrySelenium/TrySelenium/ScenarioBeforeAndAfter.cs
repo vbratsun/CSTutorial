62c7b20f6c89ad719fe9a2c51260c36d127b7c0f
vbratsun <vladimir.bratsun@fourth.com>
2016-01-14 18:09:37 +0300

    add before after scenario hook for browser disposal

---
+++ TrySelenium/TrySelenium/ScenarioBeforeAndAfter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace TrySelenium
+{
+    [Binding]
+    internal class ScenarioBeforeAndAfter
+    {
+        [BeforeScenario]
+        public static void Before()
+        {
+            //runs before each scenario and scenario outline example
+        }
+
+        [AfterScenario]
+        public static void After()
+        {
+            WebBrowser.Current.Dispose();
+        }
+
+    }
+}
