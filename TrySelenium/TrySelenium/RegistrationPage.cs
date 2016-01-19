3eeda2c33964672b86d93e1bf55b98c0ee9c8c6a
vbratsun <vladimir.bratsun@fourth.com>
2016-01-19 18:19:42 +0300

    trying to add page object

---
+++ TrySelenium/TrySelenium/RegistrationPage.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TrySelenium
+{
+    internal class RegistrationPage//: Page
+    {
+        //private readonly IHeader _header;
+
+    }
+}
