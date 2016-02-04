06ff0435efd0a9db4f48c4f18d6fae67e47f5461
vbratsun <vbratsun@gmail.com>
2016-02-04 23:50:08 +0300

    NoLuck

--- PythonStart/DateTimeCheck.py
+++ PythonStart/DateTimeCheck.py
@@ -1,45 +1,52 @@
 from datetime import datetime, date, time
 from builtins import print
 
+notebookFile = 'NotebookTest.txt'
+
+#def fileExistCheck():
+
+
 
 def main():
     print(date.today())
     print("To check if anyone has birthday type - Bday")
 
-while True:
-    action = input()
-    expected = "Bday"
-    if expected == action:
-        try:
-            file = open('NotebookTest.txt','r')
-        except IOError as e:
-            file = open('NotebookTest.txt','w')
-            file.close()
-            print("Error: File with data is not found!")
-            print("*Use AddNew command to create first record in new data file")
-        else:
-            with file:
-                file = open('NotebookTest.txt','r')
-                list = [line.strip() for line in file]
+    while True:
+        action = input()
+        expected = "Bday"
+        if expected == action:
+            try:
+                file = open(notebookFile,'r')
+            except IOError as e:
+                file = open(notebookFile,'w')
                 file.close()
-                #print(list) #просто вывод списка
-                rowCount = len(list)
-                print()
-                if rowCount==0:
-                    print("Notebook is empty.")
-                else:
-                    birthdayCounter=0
-                    dateToday=str(date.today())
-                    partOfDate = dateToday[4:10]
+                print("Error: File with data is not found!")
+                print("*Use AddNew command to create first record in new data file")
+            else:
+                with file:
+                    file = open(notebookFile,'r')
+                    list = [line.strip() for line in file]
+                    file.close()
+                    #print(list) #просто вывод списка
+                    rowCount = len(list)
+                    print()
+                    if rowCount==0:
+                        print("Notebook is empty.")
+                    else:
+                        birthdayCounter=0
+                        dateToday=str(date.today())
+                        partOfDate = dateToday[4:10]
 
-                    for record in list:
-                        if partOfDate in record:
-                            birthdayCounter=birthdayCounter+1
-                            print(record.replace("|"," "))
+                        for record in list:
+                            if partOfDate in record:
+                                birthdayCounter=birthdayCounter+1
+                                print(record.replace("|"," "))
 
-                    if birthdayCounter==0:
-                        print("No one has birthday today.")
-                        print()
+                        if birthdayCounter==0:
+                            print("No one has birthday today.")
+                            print()
 
-    else:
-        print("Error: No such command. Please try again.")
+        else:
+            print("Error: No such command. Please try again.")
+
+main()
---
+++ PythonStart/Experiments.py
@@ -0,0 +1,59 @@
+from datetime import datetime, date, time
+from builtins import print
+
+notebookFile = 'ZZZ.txt'
+
+def fileExistCheck(n):
+    try:
+        file = open(notebookFile,'r')
+    except IOError as e:
+        file = open(notebookFile,'w')
+        file.close()
+        print("Error: File with data is not found!")
+        print("*Use AddNew command to create first record in new data file")
+        return 0
+    else:
+        with file:
+            return notebookFile
+
+def fileIsEmpty(n):
+    file = open(notebookFile,'r')
+    singleLine = [line.strip() for line in file]
+    file.close()
+    rowCount = len(singleLine)
+    print()
+    if rowCount==0:
+        print("Notebook is empty.")
+        return 0
+    else:
+     return singleLine
+
+def main():
+    print(date.today())
+    print("To check if anyone has birthday type - Bday")
+
+    while True:
+        action = input()
+        expected = "Bday"
+        if expected == action:
+            if fileExistCheck(notebookFile)!=0:
+                birthdayCounter=0
+                dateToday=str(date.today())
+                partOfDate = dateToday[4:10]
+                if fileIsEmpty(notebookFile)!=0:
+                    for record in singleLine:
+                        if partOfDate in record:
+                            birthdayCounter=birthdayCounter+1
+                            print(record.replace("|"," "))
+
+                    if birthdayCounter==0:
+                        print("No one has birthday today.")
+                        print()
+
+
+
+
+        else:
+            print("Error: No such command. Please try again.")
+
+main()
